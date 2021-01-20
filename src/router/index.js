ada1e5cb165cb70a885201e62258130f7e2e287b
SmaVivian <vivi163abc@163.com>
2021-01-20 15:25:14 +0800

    history模式下页面切换时的滚动行为

--- src/router/index.js
+++ src/router/index.js
@@ -89,7 +89,15 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    // return 期望滚动到哪个的位置
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 export default router
